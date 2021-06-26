6d5843f46c7c3a34279e75d1970f2997f3fe825a
miLLlog <milllog@mail.ru>
2021-06-27 02:03:25 +0300

    Update hw2.js

--- hw2.js
+++ hw2.js
@@ -3,7 +3,8 @@ import pictures from './gallery-items.js'
 
 const cardsContainer = document.querySelector('.js-gallery')
 const makeModalOpen = document.querySelector('.js-lightbox')
-
+const makeModalClose = document.querySelector('.lightbox__button')
+const openOriginalImg = document.querySelector('.lightbox__image')
 
 
 const cardsMarkup = createGalleryCardsMarkup(pictures)
@@ -12,10 +13,14 @@ cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup)
 
 cardsContainer.addEventListener('click', onCardsContainerClick)
 
+makeModalClose.addEventListener('click', () => {
+  makeModalOpen.classList.remove('is-open');
+  
+})
 
 function createGalleryCardsMarkup (pictures) {
   return pictures.map( ({preview, original, description}) => {
-     return `
+    return `
      <li class="gallery__item">
         <a
           class="gallery__link"
@@ -34,6 +39,10 @@ function createGalleryCardsMarkup (pictures) {
 }
 
 function onCardsContainerClick(e) {
-  console.log(e.target);
+  e.preventDefault();
+  if (e.target.classList.contains('gallery__image')) {
+    makeModalOpen.classList.add('is-open')
+  openOriginalImg.src = ''
+  openOriginalImg.src = e.target.dataset.source
+  }
 }
-
